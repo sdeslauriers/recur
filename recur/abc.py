f92258d27ff9738801df28a8a718f307c9fda077
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2017-08-11 11:03:14 +0200

    Add MultiRecusive abstract base class
    The Recursive ABC handles recursive structures that contain
    a single iterable of Recursive instances. However, certain
    structures contain several lists of iterables and thus have
    several ways be iterated.
    
    This commit adds a new ABC called MultiRecursive which handles
    this case. Iterating over instances of MultiRecursive currently
    requires manually creating the iterator. This will be improved
    in the future.

--- recur/abc.py
+++ recur/abc.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from collections.abc import Iterator
 
 
 class Recursive(ABC):
@@ -22,6 +23,68 @@ class Recursive(ABC):
         yield self
 
 
+class MultiRecursive(Recursive):
+    """Abstract base class for classes that provide __multirecur__ method
+
+    This abstract base class can be used to generate data structures where
+    and instance can provide several lists of instances of the same class.
+    A simple example is a graph which can provide an list of children and
+    a list of parents.
+
+    """
+
+    @abstractmethod
+    def __multirecur__(self, index):
+        """Returns the ith iterable of instances of MultiRecursive"""
+        pass
+
+    def __recur__(self):
+        return self.__multirecur__(0)
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+
+        # Must meet the requirements of Recursive.
+        if not super().__subclasshook__(subclass):
+            return False
+
+        # and have __multirecur__.
+        if not any('__multirecur__' in s.__dict__ for s in subclass.__mro__):
+            return False
+
+        return True
+
+
+class MultiRecursiveIterator(Iterator):
+    """Iterator for MultiRecursive instances"""
+
+    PREORDER = 'pre'
+    POSTORDER = 'post'
+
+    def __init__(self, multirecursive, index, order):
+        super().__init__()
+
+        if not isinstance(multirecursive, MultiRecursive):
+            raise TypeError(
+                '\'multirecursive\' must be an instance of {} or implement '
+                'the __recur__ and __multirecur__ methods')
+        self.multirecursive = multirecursive
+
+        if order != self.PREORDER and order != self.POSTORDER:
+            raise ValueError('\'order\' must be {} or {}, not {}'
+                             .format(self.PREORDER, self.POSTORDER, order))
+        self.order = order
+
+        self.index = index
+        self.nextfun = (n for n in _nextfun(self))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.nextfun)
+
+
 def postorder(func):
     """Decorator function for methods that iterate in postorder"""
     def nested(obj, *args):
@@ -36,3 +99,16 @@ def preorder(func):
         for subobj in obj:
             func(subobj, *args)
     return nested
+
+
+def _nextfun(iter):
+
+    if iter.order == 'pre':
+        yield iter.multirecursive
+
+    items = iter.multirecursive.__multirecur__(iter.index)
+    for item in items:
+        yield from MultiRecursiveIterator(item, iter.index, iter.order)
+
+    if iter.order == 'post':
+        yield iter.multirecursive
--- recur/test/test_abc.py
+++ recur/test/test_abc.py
@@ -1,6 +1,7 @@
 import unittest
 
 from recur import postorder, preorder
+from recur.abc import MultiRecursiveIterator
 import recur.tree
 
 
@@ -45,3 +46,121 @@ class Decorators(unittest.TestCase):
         flat = []
         root.reverse_flatten(flat)
         self.assertEqual(flat, [l for l in 'abcd'])
+
+
+class MultiRecursiveSubClass(recur.abc.MultiRecursive):
+    """A mockup class to test the MultiRecursive ABC"""
+
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+        self.links = ([], [])  # (children, parents)
+
+    def __multirecur__(self, index):
+        return self.links[index]
+
+
+class TestMultiRecursive(unittest.TestCase):
+
+    def test_recur_simple(self):
+        """Test that MultiRecursive behaves like Recursive"""
+
+        nodes = [MultiRecursiveSubClass(i) for i in range(4)]
+        for n1, n2 in zip(nodes[:-1], nodes[1:]):
+            n1.links[0].append(n2)
+            n2.links[1].append(n1)
+
+        output = [n.value for n in nodes[0]]
+        self.assertListEqual(output, [0, 1, 2, 3])
+
+        output = [n.value for n in reversed(nodes[0])]
+        self.assertListEqual(output, [3, 2, 1, 0])
+
+    def test_multi_recursive_iterator_simple_list(self):
+        """Test the MultiRecursiveIterator for a list
+
+        This test verifies that the MultiRecursiveIterator can traverse
+        lists in pre and post order and for ancestors and descendants.
+
+        """
+
+        nodes = [MultiRecursiveSubClass(i) for i in range(4)]
+        for n1, n2 in zip(nodes[:-1], nodes[1:]):
+            n1.links[0].append(n2)
+            n2.links[1].append(n1)
+
+        iterator = MultiRecursiveIterator(nodes[0], 0, 'pre')
+        output = [n.value for n in iterator]
+        self.assertListEqual(output, [0, 1, 2, 3])
+
+        iterator = MultiRecursiveIterator(nodes[0], 0, 'post')
+        output = [n.value for n in iterator]
+        self.assertListEqual(output, [3, 2, 1, 0])
+
+        iterator = MultiRecursiveIterator(nodes[-1], 1, 'pre')
+        output = [n.value for n in iterator]
+        self.assertListEqual(output, [3, 2, 1, 0])
+
+        iterator = MultiRecursiveIterator(nodes[-1], 1, 'post')
+        output = [n.value for n in iterator]
+        self.assertListEqual(output, [0, 1, 2, 3])
+
+    def test_multi_recursive_iterator_tree(self):
+        """Test the MultiRecursiveIterator for a tree
+
+        This test verifies that the MultiRecursiveIterator can traverse
+        a tree in pre/post order and for ancestors and descendants.
+
+        """
+
+        nodes = [MultiRecursiveSubClass(i) for i in range(5)]
+
+        nodes[0].links[0].append(nodes[1])
+        nodes[1].links[1].append(nodes[0])
+
+        nodes[0].links[0].append(nodes[2])
+        nodes[2].links[1].append(nodes[0])
+
+        nodes[2].links[0].append(nodes[3])
+        nodes[3].links[1].append(nodes[2])
+
+        nodes[2].links[0].append(nodes[4])
+        nodes[4].links[1].append(nodes[2])
+
+        # Descendents, preorder.
+        iterator = MultiRecursiveIterator(nodes[0], 0, 'pre')
+        output = [n.value for n in iterator]
+        self.assertListEqual(output, [0, 1, 2, 3, 4])
+
+        # Descendents, postorder.
+        iterator = MultiRecursiveIterator(nodes[0], 0, 'post')
+        output = [n.value for n in iterator]
+        self.assertListEqual(output, [1, 3, 4, 2, 0])
+
+        # Ancestors, preorder.
+        iterator = MultiRecursiveIterator(nodes[-1], 1, 'pre')
+        output = [n.value for n in iterator]
+        self.assertListEqual(output, [4, 2, 0])
+
+        # Ancestors, postorder.
+        iterator = MultiRecursiveIterator(nodes[-1], 1, 'post')
+        output = [n.value for n in iterator]
+        self.assertListEqual(output, [0, 2, 4])
+
+
+class TestMultiRecursiveIterator(unittest.TestCase):
+
+    def test_init(self):
+        """Test the __init__ method of the MultiRecursiveIterator class"""
+
+        class ValidRecur(object):
+            def __multirecur__(self):
+                pass
+
+        # Must get an object that implements __multirecur__.
+        self.assertRaises(TypeError, MultiRecursiveIterator,
+                          None, 0, 'pre')
+
+        # Must get pre or post for the order'.
+        self.assertRaises(ValueError, MultiRecursiveIterator,
+                          ValidRecur(), 0, 'a')
